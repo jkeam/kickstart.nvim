c79f88f09450effdbf37054241bcf84648a0176e
Jon Keam <jpkeam@gmail.com>
2026-04-02 20:47:13 -0400

    set theme

--- init.lua
+++ init.lua
@@ -851,8 +851,8 @@ require('lazy').setup({
 
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- any other, such as 'tokyonight-night', 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.cmd.colorscheme 'sonokai'
     end,
   },
 
